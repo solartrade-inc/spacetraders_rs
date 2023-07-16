ab5f33e34052576f5c9d7b90c6c89590fddca6d6
Justin Auger <justinauger@gmail.com>
2023-07-16 15:21:57 +0100

    chkpt working.

--- src/ratio_optimiser.rs
+++ src/ratio_optimiser.rs
@@ -39,102 +39,168 @@ enum EdgeType {
     Probability(f64),
 }
 
-fn evaluate(graph: &DirectedCsrGraph<usize, (), Edge<Metric>>) {
-    // dfs
-    #[derive(Clone, Debug, PartialEq)]
-    enum Color {
-        Unvisited,
-        Visited,
-        Exited,
-    }
-    let mut stack: Vec<(usize, bool)> = vec![];
-    let mut color: Vec<Color> = vec![Color::Unvisited; graph.node_count()];
+// fn evaluate(graph: &DirectedCsrGraph<usize, (), Edge<Metric>>) {
+//     // dfs
+//     #[derive(Clone, Debug, PartialEq)]
+//     enum Color {
+//         Unvisited,
+//         Visited,
+//         Exited,
+//     }
+//     let mut stack: Vec<(usize, bool)> = vec![];
+//     let mut color: Vec<Color> = vec![Color::Unvisited; graph.node_count()];
 
-    let mut sum: Vec<Metric> = vec![Metric::default(); graph.node_count()];
+//     let mut sum: Vec<Metric> = vec![Metric::default(); graph.node_count()];
 
-    let INTERVAL_START = f64::MIN;
-    let INTERVAL_END = f64::MAX;
+//     let INTERVAL_START = f64::MIN;
+//     let INTERVAL_END = f64::MAX;
 
-    // (start, end, x, t)
-    let mut interval: Vec<(f64, f64, f64, f64)> = vec![];
-    let mut interval_idx: Vec<Option<(usize, usize)>> = vec![None; graph.node_count()];
+//     // (start, end, x, t)
+//     let mut interval: Vec<(f64, f64, f64, f64)> = vec![];
+//     let mut interval_idx: Vec<Option<(usize, usize)>> = vec![None; graph.node_count()];
 
-    stack.push((0, false));
-    while let Some((x, is_exit)) = stack.pop() {
-        match is_exit {
-            false => {
-                if color[x] != Color::Unvisited {
-                    continue;
-                }
-                color[x] = Color::Visited;
-                println!("A {}", x);
-                stack.push((x, true));
-                for &t in graph.out_neighbors_with_values(x).rev() {
-                    let y = t.target;
-                    let edge = t.value;
-                    // in forward pass, we sum the metric
-                    sum[y] = sum[x] + edge.metric;
-                    stack.push((y, false));
-                }
-            }
-            true => {
-                color[x] = Color::Exited;
-                println!("B {}", x);
-                assert!(interval_idx[x].is_none());
+//     stack.push((0, false));
+//     while let Some((x, is_exit)) = stack.pop() {
+//         match is_exit {
+//             false => {
+//                 if color[x] != Color::Unvisited {
+//                     continue;
+//                 }
+//                 color[x] = Color::Visited;
+//                 println!("A {}", x);
+//                 stack.push((x, true));
+//                 for &t in graph.out_neighbors_with_values(x).rev() {
+//                     let y = t.target;
+//                     let edge = t.value;
+//                     // in forward pass, we sum the metric
+//                     sum[y] = sum[x] + edge.metric;
+//                     stack.push((y, false));
+//                 }
+//             }
+//             true => {
+//                 color[x] = Color::Exited;
+//                 println!("B {}", x);
+//                 assert!(interval_idx[x].is_none());
 
-                if graph.out_degree(x) == 0 {
-                    // if leaf then single (complete) interval
-                    let idx = interval.len();
-                    interval.push((INTERVAL_START, INTERVAL_END, sum[x].0, sum[x].1));
-                    interval_idx[x] = Some((idx, idx));
-                }
-                else {
-                    // if not leaf then merge intervals
-                    let neighbors = graph.out_neighbors_with_values(x).collect::<Vec<_>>();
-                    let mut x_interval_idx = interval_idx[neighbors[0].target].unwrap();
-                    let node_type = neighbors[0].value.edge_type;
-                    println!("interval_idx[{}] = {}, {}", neighbors[0].target, x_interval_idx.0, x_interval_idx.1);
-                    for &t in graph.out_neighbors_with_values(x).skip(1) {
-                        let y = t.target;
-                        let (a, b) = interval_idx[y].unwrap();
+//                 if graph.out_degree(x) == 0 {
+//                     // if leaf then single (complete) interval
+//                     let idx = interval.len();
+//                     interval.push((INTERVAL_START, INTERVAL_END, sum[x].0, sum[x].1));
+//                     interval_idx[x] = Some((idx, idx));
+//                 }
+//                 else {
+//                     // if not leaf then merge intervals
+//                     let neighbors = graph.out_neighbors_with_values(x).collect::<Vec<_>>();
+//                     let mut x_interval_idx = interval_idx[neighbors[0].target].unwrap();
+//                     let node_type = neighbors[0].value.edge_type;
+//                     println!("interval_idx[{}] = {}, {}", neighbors[0].target, x_interval_idx.0, x_interval_idx.1);
+//                     for &t in graph.out_neighbors_with_values(x).skip(1) {
+//                         let y = t.target;
+//                         let (a, b) = interval_idx[y].unwrap();
                         
-                        println!("interval_idx[{}] = {}, {}", y, a, b);
-                        assert_eq!(x_interval_idx.1 + 1, a);
-                        assert_eq!(node_type, t.value.edge_type);
-                        println!("merging");
-                        println!("item 1:");
-                        for i in x_interval_idx.0..=x_interval_idx.1 {
-                            println!("{}: {:?}", i, interval[i]);
-                        }
-                        println!("item 2:");
-                        for i in a..=b {
-                            println!("{}: {:?}", i, interval[i]);
-                        }
+//                         println!("interval_idx[{}] = {}, {}", y, a, b);
+//                         assert_eq!(x_interval_idx.1 + 1, a);
+//                         assert_eq!(node_type, t.value.edge_type);
+//                         println!("merging");
+//                         println!("item 1:");
+//                         for i in x_interval_idx.0..=x_interval_idx.1 {
+//                             println!("{}: {:?}", i, interval[i]);
+//                         }
+//                         println!("item 2:");
+//                         for i in a..=b {
+//                             println!("{}: {:?}", i, interval[i]);
+//                         }
 
-                        // merge
+//                         // merge
 
 
-                        x_interval_idx.1 = b;
-                    }
-                    println!("x_interval_idx {:?}", x_interval_idx);
-                    interval_idx[x] = Some(x_interval_idx);
+//                         x_interval_idx.1 = b;
+//                     }
+//                     println!("x_interval_idx {:?}", x_interval_idx);
+//                     interval_idx[x] = Some(x_interval_idx);
 
-                    // actually merge
+//                     // actually merge
                     
+//                 }
+//             }
+//         }
+
+//     }
+
+//     // todo: 
+//     // we need to merge up the tree, merging leaf nodes into their parents until only the root remains
+//     // the merged nodes should use similar amount of memory, but are associated with a new single node
+
+//     // and almost done
+//     // finally compute X for each interval, and pick the greatest that aligns X = F(X)
+
+//     println!("{:?}", interval);
+// }
+
+
+// fn f(graph: &DirectedCsrGraph<usize, (), Edge<Metric>>) {
+
+fn step(graph: &DirectedCsrGraph<usize, (), Edge<Metric>>, x: usize, x0: f64) -> (f64, f64) {
+    if graph.out_degree(x) == 0 {
+        // leaf
+        // node themselves have no metric, only edges
+        return (0.0, 0.0);
+    }
+    // println!("step({}, {})", x, x0);
+    let neighbours = graph.out_neighbors_with_values(x).collect::<Vec<_>>();
+    let edge_type = neighbours[0].value.edge_type;
+    
+    let ret = match edge_type {
+        EdgeType::Decision => {
+            let mut max = (f64::MIN, f64::MIN);
+            for &t in neighbours.iter() {
+                let y = t.target;
+                let edge = t.value.metric;
+                if let EdgeType::Probability(_) = t.value.edge_type {
+                    panic!()
+                }
+                let (g, dg) = step(graph, y, x0);
+                let f = g + (edge.0 - x0 * edge.1);
+                let df = dg - edge.1;
+                if f > max.0 || f == max.0 && df > max.1{
+                    max.0 = f;
+                    max.1 = df;
                 }
             }
+            max
         }
+        EdgeType::Probability(_) => {
+            let mut sum = (0.0, 0.0);
+            let mut weight_sum = 0.0;
+            for &t in neighbours.iter() {
+                let y = t.target;
+                let edge = t.value.metric;
+                let edge_weight = match t.value.edge_type {
+                    EdgeType::Probability(w) => w,
+                    _ => panic!()
+                };
+                let (g, dg) = step(graph, y, x0);
+                let f = g + (edge.0 - x0 * edge.1);
+                let df = dg - edge.1;
+                sum.0 += f * edge_weight;
+                sum.1 += df * edge_weight;
+                weight_sum += edge_weight;
+            }
+            (sum.0 / weight_sum, sum.1 / weight_sum)
+        }
+    };
+    // println!("step({}, {}) = {:?}", x, x0, ret);
+    ret
+}
 
+fn evaluate(graph: &DirectedCsrGraph<usize, (), Edge<Metric>>) {
+    let mut x0 = 0.0;
+    for _ in 0..10 {
+        let (f, df) = step(graph, 0, x0);
+        println!("f({}) = {}, df = {}", x0, f, df);
+        x0 -= f / df;
     }
-
-    // todo: 
-    // we need to merge up the tree, merging leaf nodes into their parents until only the root remains
-    // the merged nodes should use similar amount of memory, but are associated with a new single node
-
-    // and almost done
-    // finally compute X for each interval, and pick the greatest that aligns X = F(X)
-
-    println!("{:?}", interval);
+    println!("x0 = {}", x0);
 }
 
 // #[cfg(test)]
