de9e8496cda5f99ca14bd6eb8025a954c23744af
Justin Auger <justinauger@gmail.com>
2023-07-28 10:41:04 +0100

    fixup deploy.

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ ssh $SSH_DEPLOY_TARGET -- "mkdir -p /opt/spacetraders_rs"
 
 echo "Deploying to $SSH_DEPLOY_TARGET"
 rsync -avzP target/release/run $SSH_DEPLOY_TARGET:/opt/spacetraders_rs
-rsync -avzP .env $SSH_DEPLOY_TARGET:/opt/spacetraders_rs
+rsync -avzP remote.env $SSH_DEPLOY_TARGET:/opt/spacetraders_rs/.env
 rsync -avzP deploy/spacetraders_rs.service $SSH_DEPLOY_TARGET:/etc/systemd/system/spacetraders_rs.service
 
 echo "Restarting service"
